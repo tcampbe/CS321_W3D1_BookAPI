0233e5c957621ad2681a3ff666e96178185111c6
Matt Cameron <cameron.matt@gmail.com>
2019-06-11 18:12:16 -0500

    initial commit

---
+++ CS321_W3D1_BookAPI/Services/IBookService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CS321_W3D1_BookAPI.Models;
+
+namespace CS321_W3D1_BookAPI.Services
+{
+    public interface IBookService
+    {
+        // CRUDL - create (add), read (get), update, delete (remove), list
+
+        // create
+        Book Add(Book todo); 
+        // read
+        Book Get(int id);
+        // update
+        Book Update(Book todo); 
+        // delete
+        void Remove(Book todo);
+        // list
+        IEnumerable<Book> GetAll();
+    }
+}
